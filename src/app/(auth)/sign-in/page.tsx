06187b150759bddf5b7b3f5f1ba52a03e2f115bb
theskinnyhippo <trynalearn07@gmail.com>
2025-07-21 14:29:20 +0530

    04: authentication UI

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,11 @@
+import { SignInView } from "@/modules/auth/views/sign-in-view";
+
+const Page = () => {
+    return (
+        <SignInView />
+    );
+}
+
+export default Page;
+
+// localhost:3000/auth/sign-in
---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,12 @@
+import { SignUpView } from "@/modules/auth/views/sign-up-view";
+
+
+const Page = () => {
+    return (
+        <SignUpView />
+    );
+}
+
+export default Page;
+
+// localhost:3000/auth/sign-up
