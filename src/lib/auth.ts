8a8481616ce36d319146279b063383b1aae2f9c3
theskinnyhippo <trynalearn07@gmail.com>
2025-07-22 14:07:17 +0530

    05:authentication socials

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -4,6 +4,17 @@ import { db } from "@/db"; // your drizzle instance
 import * as schema from "@/db/schema"
  
 export const auth = betterAuth({
+    
+    socialProviders: {
+        google: { 
+            clientId: process.env.GOOGLE_CLIENT_ID as string, 
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string, 
+        }, 
+        github: { 
+            clientId: process.env.GITHUB_CLIENT_ID as string, 
+            clientSecret: process.env.GITHUB_CLIENT_SECRET as string, 
+        }, 
+    },
     emailAndPassword: {
         enabled: true, 
     }, 
