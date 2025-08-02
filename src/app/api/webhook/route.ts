7c916bfb5772e99c6b7be7d1f1df8f4a67bcb6b3
theskinnyhippo <trynalearn07@gmail.com>
2025-08-02 14:58:00 +0530

    24:background jobs

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -16,6 +16,7 @@ import {
 import { db } from "@/db";
 import { agents, meetings } from "@/db/schema";
 import { streamVideo } from "@/lib/stream-video";
+import { inngest } from "@/inngest/client";
 
 
 function verifySignatureWithSDK(body : string, signature : string): boolean {
@@ -131,7 +132,81 @@ export async function POST(req : NextRequest){
         
         const call = streamVideo.video.call("default", meetingid);
         await call.end();
-    
+
+
+
+
+    } else if ( eventType === "call.session_ended"){
+        
+        const event = payload as CallEndedEvent;
+        const meetingid = event.call.custom?.meetingid;
+
+        if(!meetingid){
+            return NextResponse.json(
+                {error : "Missing meetingid"},
+                {status : 400 }
+            );
+        }
+        
+        await db
+        .update(meetings)
+        .set({
+            status : "processing",
+                endedAt : new Date()
+            })
+            .where(
+                and(
+                    eq(meetings.id, meetingid),
+                    eq(meetings.status, "active")
+                )
+            );
+            
+    } else if ( eventType === "call.transcription_ready"){
+        const event = payload as CallTranscriptionReadyEvent;
+        const meetingid = event.call_cid.split(":")[1];
+        
+        const [updatedMeeting] = await db
+            .update(meetings)
+            .set({
+                transcriptUrl : event.call_transcription.url
+            })
+            .where(eq(meetings.id, meetingid))
+            .returning();
+
+            
+        if(!updatedMeeting){
+            return NextResponse.json(
+                {error : "Meeting not found"},
+                {status : 404 }
+            );
+        }
+            
+            // call inngest to summarize
+        await inngest.send({
+            name : "meetings/processing",
+            data : {
+                meetingid : updatedMeeting.id,
+                transcriptUrl : updatedMeeting.transcriptUrl
+            }
+        })
+
+
+            
+    } else if ( eventType === "call.recording_ready"){
+        const event = payload as CallRecordingReadyEvent;
+        const meetingid = event.call_cid.split(":")[1];
+
+        await db
+            .update(meetings)
+            .set({
+                recordingUrl : event.call_recording.url
+            })
+            .where(eq(meetings.id, meetingid))
+
     }
+
     return NextResponse.json({ status : "ok" });
 }
+
+
+// localhost:8288 inngest
